06b1e9f9c496f9f7e0d12f68cf394d149d9cd4af
Sakulan <sho@adneom.lu>
2018-07-05 11:00:07 +0200

    add entity language

---
+++ src/Entity/Language.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace Fei\Service\Translate\Entity;
+
+use Fei\Entity\AbstractEntity;
+
+class Language extends AbstractEntity
+{
+    /**
+     * @var int
+     *
+     * @Id
+     * @GeneratedValue(strategy="AUTO")
+     * @Column(type="integer")
+     */
+    protected $id;
+
+    /**
+     * @var string
+     *
+     * @Column(type="string")
+     */
+    protected $name;
+
+    /**
+     * @var string
+     *
+     * @Column(type="string", name="`short_name`")
+     */
+    protected $shortName;
+
+    /**
+     * @var bool
+     *
+     * @Column(type="boolean", name="`enabled`")
+     */
+    protected $enabled;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     * @return Language
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     * @return Language
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getShortName()
+    {
+        return $this->shortName;
+    }
+
+    /**
+     * @param string $shortName
+     * @return Language
+     */
+    public function setShortName($shortName)
+    {
+        $this->shortName = $shortName;
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isEnabled()
+    {
+        return $this->enabled;
+    }
+
+    /**
+     * @param bool $enabled
+     * @return Language
+     */
+    public function setEnabled($enabled)
+    {
+        $this->enabled = $enabled;
+        return $this;
+    }
+}
